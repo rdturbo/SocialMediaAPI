c555829bb14921c122dc1e48a3e4d21b35111767
Rupayan Das <rupayan98@gmail.com>
2022-07-16 00:31:35 -0400

    database connected

---
+++ app/main.py
@@ -0,0 +1,70 @@
+import time
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+while True:
+    try:
+        conn = psycopg2.connect(
+            host="localhost", database="socialMediaAPI", user="postgres",
+            password="*", cursor_factory=RealDictCursor
+        )
+        cursor = conn.cursor()
+        print("Database was successfully connected")
+        break
+    except Exception as error:
+        print("Connection to the database failed!!!")
+        print(f"Error was {error}")
+        time.sleep(2)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/posts")
+def get_posts():
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    return {"data": posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data": new_post}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(id)))
+    post = cursor.fetchone()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
+    return {"data": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    return {"message": "post successfully updated"}
