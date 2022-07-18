fb10d105c6d6a4ba63e52e277184b16125ced0d4
Rupayan Das <rupayan98@gmail.com>
2022-07-17 23:10:58 -0400

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app import models
 from app.database import engine
 from app.config import settings
 from app.routers import post, user, auth, vote
 
-models.Base.metadata.create_all(bind=engine)
+# needed before alembic was implemented
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
